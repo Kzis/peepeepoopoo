5b992f7dc6ca08b5cfab162c9ffd97d230512acd
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-06-23 21:28:42 +0700

    Import lib to vue instance

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import VueFire from 'vuefire'
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+//include vue-material plug-in
+Vue.use(VueMaterial)
+//include vuefire plug-in
+Vue.use(VueFire)
+//include vue2-google-map
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBfKA94ZnVcppyzxqGeLYZAj4_k8IRSvTQ',
+    v: '3.26',                // Google Maps API version 
+    // libraries: 'places',   // If you want to use places input 
+  }
+})
 
 Vue.config.productionTip = false
 
