79457dc7dfc4dc8c7378e5a4f65ce4c8c668e1c2
PG-Chalat <driftkao@gmail.com>
2017-06-24 00:47:11 +0700

    Draw div wait data by form search google map api

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,7 +1,19 @@
 <template>
  
-  <div class="seaech-googlemap">
-     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  <div class="seaech-googlemap" style="height:100%;">
+  
+    <div class="row" style="margin:10px;">
+        <div class="col-sm-12" style="background-color:#cccccc; height:300px;">
+           DIV 1
+        </div>
+    </div>
+
+    <div class="row" style="margin:10px;">
+        <div class="col-sm-12" style="background-color:#cccccc; height: 300px;"> 
+            DIV 2
+        </div>
+    </div>
+
   </div>
 
 </template>
