e4930fafd666b6c3ccbebaae7dea7043a4a63c38
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-06-23 22:48:01 +0700

    Mock example map to project

--- src/components/SearchToilet.vue
+++ src/components/SearchToilet.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    <h1>Search</h1>
+  <div class="hello">
+    <gmap-map
+      :center="center"
+      :zoom="7"
+      style="width: 100%; height: 300px"
+    >
+      <gmap-marker
+        :key="index"
+         v-for="(m, index) in markers"
+        :position="m.position"
+        :icon="m.icon"
+        :clickable="true"
+      ></gmap-marker>
+    </gmap-map>
   </div>
 </template>
 
 <script>
+import svg from '../configs/svg.js'
 export default {
   name: 'search-toilet',
   data () {
     return {
-      
+      center: {lat: 11.0, lng: 10.0},
+      markers: [{
+        position: {lat: 10.0, lng: 10.0},
+       icon: svg.TOILET
+      }, {
+        position: {lat: 11.0, lng: 11.0},
+        icon: svg.TOILET,
+      }]
     }
   }
 }
@@ -19,3 +39,4 @@ export default {
 <style scoped>
 
 </style>
+
