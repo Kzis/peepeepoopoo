c1d9bd1e332efcece2f6da9993cc77a3199c33d0
PG-Chalat <driftkao@gmail.com>
2017-06-25 01:28:17 +0700

    Commit new style login

--- src/configs/images.js
+++ src/configs/images.js
@@ -1,6 +1,6 @@
 //for add all images in project
 let images = {
-    LOGO_LARGE: require('../../static/img/icons/peepeepoopoo-hires-640.png'),
+    LOGO_LARGE: require('../../static/img/icons/peepeepoopoo-hires-192.png'),
     BG:require('../../static/img/icons/bg-toilet.jpg')
 }
 
