84b859ef194d49a30476e33dc392afddbe4b4b8e
Por <pompororpor@gmail.com>
2017-06-24 20:36:09 +0700

    function js geolocation

--- src/components/SearchToilet.vue
+++ src/components/SearchToilet.vue
@@ -4,7 +4,7 @@
   <div class="hello">
     <gmap-map
       :center="center"
-      :zoom="7"
+      :zoom="zoomNumber"
       style="width: 100%; height: 300px"
     >
       <gmap-marker
@@ -14,9 +14,16 @@
         :icon="m.icon"
         :clickable="true"
       ></gmap-marker>
+      <gmap-marker
+        :position="makerMe.position"
+        :icon="makerMe.icon"
+        :clickable="true"
+      ></gmap-marker>
     </gmap-map>
   </div>
-  <button @click="locationNew">update location</button>
+<md-button class="md-icon-button md-raised md-primary" @click="locationNew">
+  <md-icon>add</md-icon>
+</md-button>
 
 </div>
 
@@ -24,6 +31,7 @@
 
 <script>
 import svg from '../configs/svg.js'
+
 export default {
   name: 'search-toilet',
   data () {
@@ -35,61 +43,74 @@ export default {
       }, {
         position: {lat: 11.0, lng: 11.0},
         icon: svg.TOILET,
-      }]
+      }],
+      makerMe : {
+        icon: svg.positionMe
+      },
+      zoomNumber : 7,
+      intevalIdGeolocation : undefined,
     }
   },
   methods: {
     locationNew : function() {
-      if(navigator.geolocation) {
         var self = this;
-       setInterval(function(){
-         navigator.geolocation.getCurrentPosition(function(position){
-          console.log("auto geolocation");
-        self.markers = [{position: {lat: position.coords.latitude, lng: position.coords.longitude}, icon: svg.TOILET }];
-self.center = {lat: position.coords.latitude, lng: position.coords.longitude};
-       });
-         console.log("update location")}, 1500);
+        var first = true;
+        if(self.intevalIdGeolocation){
+          clearInterval(self.intevalIdGeolocation);
+          console.log("clear interval");
+        }
+
+      if(navigator.geolocation) {
+        var options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        }
+        function success(pos) {
+          var crd = pos.coords;
+          //var point = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+          console.log('Your current position is:');
+          console.log(`Latitude : ${crd.latitude}`);
+          console.log(`Longitude: ${crd.longitude}`);
+          console.log(`More or less ${crd.accuracy} meters.`);
+          console.log(`Latitude : ${self.center.lat}`);
+          console.log(`Longitude : ${self.center.lng}`);
+          if(first){
+            if(self.center.lat === crd.latitude && self.center.lng === crd.longitude){
+              console.log("equal positon");
+              self.center = {lat: crd.latitude + 10.0, lng: crd.longitude + 10.0}
+            }else{
+              console.log("not equal positon");
+              self.center = {lat: crd.latitude, lng: crd.longitude};
+            }
+            
+            setTimeout(function(){
+              self.center = {lat: crd.latitude, lng: crd.longitude};
+              self.zoomNumber = 16;
+            }, 10);
+            first = false;
+          }
+
+          self.makerMe.position = {lat: crd.latitude, lng: crd.longitude};
+        }
+        function error(err) {
+          console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+        navigator.geolocation.getCurrentPosition(success, error, options);
+        self.intevalIdGeolocation = setInterval(function(){ 
+          navigator.geolocation.getCurrentPosition(success, error, options);
+        }, 10000);
       }
       //this.center = {lat: 5.0, lng: 5.0};
     },
-    autoLocation : function() {
-      //var startPos;
-      //var nudge = document.getElementById("nudge");
-
-      //var showNudgeBanner = function() {
-      //  nudge.style.display = "block";
-      //};
-
-      //var hideNudgeBanner = function() {
-      //  nudge.style.display = "none";
-      //};
-
-      var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
-
-      var geoSuccess = function(position) {
-        //hideNudgeBanner();
-        // We have the location, don't display banner
-        clearTimeout(nudgeTimeoutId);
-
-        // Do magic with location
-        //startPos = position;
-        console.log("new location");
-        //document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-        //document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-      };
-      var geoError = function(error) {
-        switch(error.code) {
-          case error.TIMEOUT:
-            // The user didn't accept the callout
-            showNudgeBanner();
-            break;
-        }
-      };
-
-      navigator.geolocation.watchPosition(geoSuccess, geoError);
+    changeLocation : function() {
+      var self = this;
+      self.center = {lat: 11.0, lng: 10.0};
     }
   }
 }
+
+
 </script>
 
 
